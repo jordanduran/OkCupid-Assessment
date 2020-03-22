02adce0b80b8de247c2fcefdb9c61c6e31e92aaa
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-03-22 19:58:10 -0400

    Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,11 +1,72 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { submitField, startOverButtonClicked } from '../../madlibs';
+import PropTypes from 'prop-types';
 
-const Form = () => {
+const Form = ({ showEditForm, essayText, dispatch }) => {
+  const handleBlur = e =>
+    dispatch(submitField({ id: e.target.name, answer: e.target.value }));
+
   return (
     <form>
-      <h1>Form</h1>
+      {showEditForm ? (
+        <fieldset className='start-over-form'>
+          <legend>Your essay text</legend>
+          <textarea defaultValue={essayText}></textarea>
+          <button
+            id='start-over-btn'
+            onClick={() => dispatch(startOverButtonClicked())}
+          >
+            Start over
+          </button>
+        </fieldset>
+      ) : (
+        <fieldset>
+          <legend>About Me</legend>
+          <label htmlFor='hometown'>Where did you grow up?</label>
+          <input
+            id='hometown'
+            type='text'
+            name='hometown'
+            onBlur={handleBlur}
+          />
+          <label htmlFor='favoriteFood'>What's your favorite food?</label>
+          <input
+            id='favoriteFood'
+            type='text'
+            name='favoriteFood'
+            onBlur={handleBlur}
+          />
+          <label htmlFor='loveToDo'>What do you LOVE to do?</label>
+          <input
+            id='loveToDo'
+            type='text'
+            name='loveToDo'
+            onBlur={handleBlur}
+          />
+          <label htmlFor='messageIf'>
+            People should message you if they...
+          </label>
+          <input
+            id='messageIf'
+            type='text'
+            name='messageIf'
+            onBlur={handleBlur}
+          />
+        </fieldset>
+      )}
     </form>
   );
 };
 
-export default Form;
+Form.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  essayText: PropTypes.string.isRequired,
+  showEditForm: PropTypes.bool.isRequired
+};
+
+function mapStateToProps(state) {
+  return state;
+}
+
+export default connect(mapStateToProps)(Form);
