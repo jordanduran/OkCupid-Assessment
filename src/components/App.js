764382d6666f72e325e186b023d0db1eb96c1538
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-03-22 19:56:52 -0400

    paragraph to display essaytext

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Form from './Form/Form';
+import { editButtonClicked } from '../madlibs';
 
 require('./App.scss');
 
-const App = () => (
+const App = ({ essayText, showEditForm, showEditButton, dispatch }) => (
   <div className='app'>
     <Form />
+    {!showEditForm && (
+      <div>
+        <h3>Your essay text</h3>
+        <p>{essayText}</p>
+        {showEditButton && (
+          <button onClick={() => dispatch(editButtonClicked())}>Edit</button>
+        )}
+      </div>
+    )}
   </div>
 );
 
 App.propTypes = {
-  dispatch: PropTypes.func.isRequired
+  dispatch: PropTypes.func.isRequired,
+  essayText: PropTypes.string.isRequired,
+  showEditForm: PropTypes.bool.isRequired,
+  showEditButton: PropTypes.bool.isRequired
 };
 
 function mapStateToProps(state) {
